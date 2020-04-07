6a4b73572c7cccffc5771dd676725a21fd7e4d52
GalinaDushanski000 <galinadusanski@yahoo.com>
2020-04-07 13:04:45 +0200

    taxReturn method finished

--- Item.java
+++ Item.java
@@ -6,10 +6,58 @@ class Item {
 	//TODO add variable.
         char danok;
 	//TODO constructor
+         public  Item(int id, String name, double price, char danok){
+         this.id=id;
+         this.name=name;
+         this.price=price;
+         this.danok=danok;
+         }
 
 	//TODO setters and getters
 
-	double taxReturn () {
-		//TODO
+        public int getId() {
+		return id;
 	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public char getDanok() {
+		return danok;
+	}
+
+	public void setDanok(char danok) {
+		this.danok = danok;
+	}
+	
+	double taxReturn () {
+		double tax;
+		if(danok=='A') {
+			tax=0.18 *price;
+			return tax*0.15;
+		}
+		else if(danok=='B') {
+			tax=0.05 *price;
+			return tax*0.15;
+		}
+		else return 0;
+	}
+	
 }
